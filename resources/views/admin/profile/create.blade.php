6a214b9445825104cf398d19896321b5eeed5e8a
村上晴 <hkw355@gmai.com>
2021-01-06 10:14:41 +0000

    commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>課題2</title>
+        <title>プロフィール</title>
     </head>
     <body>
-        <h1>create</h1>
+        <h1>プロフィール</h1>
     </body>
 </html>
