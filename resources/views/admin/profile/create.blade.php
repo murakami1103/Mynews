360f221677a2ba9005026830bb032675126dc144
村上晴 <hkw355@gmai.com>
2021-01-10 04:06:26 +0000

    commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
+@section('title', 'プロフィールの新規作成')
 
-        <title>プロフィール</title>
-    </head>
-    <body>
-        <h1>プロフィール</h1>
-    </body>
-</html>
+@section('content')
+<h1>Myプロフィール</h1>
+@endsection
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
+@section('title', 'プロフィールの編集')
 
-        <title>課題1</title>
-    </head>
-    <body>
-        <h1>edit</h1>
-    </body>
-</html>
+@section('content')
+<h1>プロフィールの編集</h1>
+@endsection
